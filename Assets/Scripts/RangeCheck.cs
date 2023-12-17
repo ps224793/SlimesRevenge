46d64d456604b43e615606489df7e2f752117749
BramGit12 <125472993+BramGit12@users.noreply.github.com>
2023-12-17 15:21:51 +0100

    comments en kleine reworks

--- Assets/Scripts/RangeCheck.cs
+++ Assets/Scripts/RangeCheck.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class RangeCheck : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Als de Speler zich binnen deze trigger bevind, zet dan de inRange Bool van de ouder van deze trigger op true
         if (collision.tag == "Player")
         {
             gameObject.GetComponentInParent<EnemyBehavior>().inRange = true;
@@ -20,8 +14,10 @@ public class RangeCheck : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
+        //Als de Speler deze trigger verlaat, zet dan de inRange Bool van de ouder van deze trigger op false
         if (collision.tag == "Player")
         {
+            // Try catch om een null reference te voorkomen bij het herladen van de scene
             try
             {
                 gameObject.GetComponentInParent<EnemyBehavior>().inRange = false;
